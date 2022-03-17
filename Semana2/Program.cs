f7fb2990ef7491c2cc2782b00ce76cd1e291898f
Caio Ferrari <caioferrari0484@gmail.com>
2022-03-16 21:19:39 -0300

    Atividade Semana2.2

---
+++ Semana2/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.BackgroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.Black;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Olá, mundo em " + DateTime.Now);
+
+
